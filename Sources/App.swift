c5ac0d95a29dcd27ee6d835e409e8fb41a6a5416
Tim Shadel <tim@shadelsoftware.com>
2017-01-21 22:33:40 -0700

    Fixup file layout

---
+++ Sources/App.swift
@@ -0,0 +1,24 @@
+//
+//  App.swift
+//  pristine
+//
+//  Created by Tim on 1/21/17.
+//  Copyright © 2017 Day Logger, Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain class App: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+
+    // MARK: - Application Lifecycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
